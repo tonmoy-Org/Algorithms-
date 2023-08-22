90a796ca09ce2156ffab4377c1fa7e15f4314356
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2023-08-23 00:44:54 +0600

    Initial Commit

--- A1.c
+++ A1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
-    int arr[] = {9, 3, 4, 2, 1, 5, 7, 8};
-    int n = 8;
+    int arr[] = {6, 9, 3, 4, 2, 1, 5, 7, 8};
+    int n = 9;
     int even_start = 0;
-    // Rearrange array so even numbers come before odd numbers
+    
     for (int i = 0; i < n; i++) {
         if (arr[i] % 2 == 0) {
             int temp = arr[even_start];
@@ -12,7 +12,7 @@ int main() {
             even_start++;
         }
     }
-    // Selection sort for even numbers in ascending order
+    
     for (int i = 0; i < even_start - 1; i++) {
         int min_index = i;
         for (int j = i + 1; j < even_start; j++) {
@@ -20,13 +20,11 @@ int main() {
                 min_index = j;
             }
         }
-        if (min_index != i) {
             int temp = arr[i];
             arr[i] = arr[min_index];
             arr[min_index] = temp;
-        }
     }
-    // Print the rearranged and sorted array
+    
     for (int k = 0; k < n; k++) {
         printf("%d ", arr[k]);
     }
